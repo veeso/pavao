3986df5be0f6504bfef3757c1a7d8674146ee063
Rui Chen <rui@chenrui.dev>
2024-12-19 10:03:20 -0500

    chore(build): improve macOS linking for libsmbclient
    Co-authored-by: veeso <christian.visintin@veeso.dev>

--- build.rs
+++ build.rs
@@ -8,24 +8,31 @@ fn main() {
         riscv64: { target_arch = "riscv64" },
         x86_64: { target_arch = "x86_64" },
         linux: { target_os = "linux" },
+        macos: { target_os = "macos" },
         // exclusive features
-        linux_aarch64: { all(linux, aarch64) },
-        linux_riscv64: { all(linux, riscv64) },
-        linux_x86_64: { all(linux, x86_64) },
-        linux_arm: { all(linux, arm) },
+        linux_aarch64: { all(linux, aarch64, unix) },
+        linux_riscv64: { all(linux, riscv64, unix) },
+        linux_x86_64: { all(linux, x86_64, unix) },
+        linux_arm: { all(linux, arm, unix) },
     }
 
     match pkg_config::find_library("smbclient") {
         Ok(_) => {
             if cfg!(target_os = "macos") {
-                println!("cargo:rustc-flags=-L /usr/local/lib -l smbclient");
+                if cfg!(target_arch = "aarch64") {
+                    println!("cargo:rustc-link-search=/opt/homebrew/opt/samba/lib");
+                } else if cfg!(target_arch = "x86_64") {
+                    println!("cargo:rustc-link-search=/usr/local/Homebrew/opt/samba/lib");
+                }
+                println!("cargo:rustc-link-lib=smbclient");
             } else {
-                println!("cargo:rustc-flags=-l smbclient");
+                println!("cargo:rustc-link-lib=smbclient");
             }
         }
         Err(e) => {
             println!(
-                "error: SMB Client library not found! Probably libsmbclient is not installed."
+                "error: SMB Client library not found! Make sure libsmbclient is installed. \
+                For macOS, install it via Homebrew with `brew install samba`."
             );
             panic!("{}", e);
         }
