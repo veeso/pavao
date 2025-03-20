75d8cebae31f6eb073a8745661da22a235746cca
Christian Visintin <christian.visintin@veeso.dev>
2025-03-20 14:43:44 +0100

    fix: Vendored libsmbclient static (#32)
    fix #20

---
+++ pavao-src/README.md
@@ -0,0 +1,82 @@
+# pavao-src
+
+Contains the source code to build the libsmbclient statically to vendor it into the pavao-sys crate.
+
+<p align="center">
+  <a href="https://www.gnu.org/licenses/gpl-3.0"
+    ><img
+      src="https://img.shields.io/badge/License-GPLv3-blue.svg"
+      alt="License-GPLv3"
+  /></a>
+  <a href="https://github.com/veeso/pavao/stargazers"
+    ><img
+      src="https://img.shields.io/github/stars/veeso/pavao.svg?style=plain"
+      alt="Repo stars"
+  /></a>
+  <a href="https://crates.io/crates/pavao-src"
+    ><img
+      src="https://img.shields.io/crates/d/pavao-src.svg"
+      alt="Downloads counter"
+  /></a>
+  <a href="https://crates.io/crates/pavao-src"
+    ><img
+      src="https://img.shields.io/crates/v/pavao-src.svg"
+      alt="Latest version"
+  /></a>
+  <a href="https://ko-fi.com/veeso">
+    <img
+      src="https://img.shields.io/badge/donate-ko--fi-red"
+      alt="Ko-fi"
+  /></a>
+</p>
+</p>
+
+## Requirements
+
+**these packages should be necessary on Linux**:
+
+- python3
+- make
+- build-essential
+- pkg-config
+- libbsd-dev
+- libcap-dev
+- libcups2-dev
+- libgnutls28-dev
+- libicu-dev
+- libjansson-dev
+- libkeyutils-dev
+- libldap2-dev
+- zlib1g-dev
+- libpam0g-dev
+- libacl1-dev
+- libarchive-dev
+- flex
+- bison
+- libntirpc-dev
+- libtracker-sparql-3.0-dev
+- libglib2.0-dev
+- libdbus-1-dev
+
+and these on **MacOS**:
+
+- pkg
+- libbsd
+- libgit2
+- icu4c
+- jansson
+- gnutls
+- zlib
+- openldap
+- cpanminus
+- libarchive
+- flex
+- bison
+- libtirpc
+- cups
+
+## License 📃
+
+Pavão-src is licensed under the GPLv3 license.
+
+You can read the entire license [HERE](LICENSE)
--- pavao-sys/build.rs
+++ pavao-sys/build.rs
@@ -1,4 +1,16 @@
 fn main() {
+    #[cfg(feature = "vendored")]
+    {
+        build_vendored();
+    }
+    #[cfg(not(feature = "vendored"))]
+    {
+        build_normal();
+    }
+}
+
+#[allow(dead_code)]
+fn build_normal() {
     match pkg_config::find_library("smbclient") {
         Ok(_) => {
             if cfg!(target_os = "macos") {
@@ -18,6 +30,89 @@ fn main() {
                 For macOS, install it via Homebrew with `brew install samba`."
             );
             panic!("{}", e);
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn build_vendored() {
+    #[cfg(feature = "vendored")]
+    build_samba();
+
+    // add further dependencies
+    add_library("z", "zlib");
+    add_library("ldap", "openldap");
+    add_library("cups", "cups");
+    add_library("lber", "openldap");
+    add_library("jansson", "jansson");
+    add_library("icui18n", "icu4c");
+    add_library("icuuc", "icu4c");
+    add_library("gnutls", "gnutls");
+    add_library("bsd", "libbsd");
+    add_library("resolv", "libresolv");
+
+    // linux only
+    if cfg!(target_os = "linux") {
+        add_library("cap", "cap");
+        add_library("keyutils", "keyutils");
+    }
+}
+
+#[cfg(feature = "vendored")]
+fn build_samba() {
+    let mut build = pavao_src::Build::new();
+
+    println!("building vendored samba library... this may take several minutes");
+    let artifacts = build.build();
+    println!("cargo:vendored=1");
+    println!(
+        "cargo:root={}",
+        artifacts.lib_dir.parent().unwrap().display()
+    );
+
+    if !artifacts.lib_dir.exists() {
+        panic!(
+            "samba library does not exist: {}",
+            artifacts.lib_dir.display()
+        );
+    }
+    if !artifacts.include_dir.exists() {
+        panic!(
+            "samba include directory does not exist: {}",
+            artifacts.include_dir.display()
+        );
+    }
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        artifacts.lib_dir.display()
+    );
+    println!("cargo:include={}", artifacts.include_dir.display());
+    println!("cargo:rustc-link-lib=static=smbclient");
+}
+
+fn add_library(lib: &str, brew_name: &str) {
+    // search lib with pkg-config and try static
+
+    match pkg_config::Config::new()
+        .statik(true)
+        .cargo_metadata(true)
+        .probe(lib)
+    {
+        Ok(_) => {
+            if cfg!(target_os = "macos") {
+                if cfg!(target_arch = "aarch64") {
+                    println!("cargo:rustc-link-search=/opt/homebrew/opt/{brew_name}/lib");
+                } else if cfg!(target_arch = "x86_64") {
+                    println!("cargo:rustc-link-search=/usr/local/Homebrew/opt/{brew_name}/lib");
+                }
+                println!("cargo:rustc-link-lib={lib}");
+            }
+        }
+        Err(_) => {
+            println!("{lib} was not found with pkg_config; trying with LD_LIBRARY_PATH; but you may need to install it manually");
+            // cross-finger and try dylib
+            println!("cargo:rustc-link-lib={lib}");
         }
     };
 }
